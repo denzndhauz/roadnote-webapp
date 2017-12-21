a26b2271c29b69053ae7bc62f83cb8f6aa030a8c
Arman <armanarco@gmail.com>
2017-12-21 22:55:34 +0800

    temporary map for home.html

--- assets/app/controllers/main.controller.js
+++ assets/app/controllers/main.controller.js
@@ -6,7 +6,7 @@
 	    .controller('MainCtrl', MainController);
 
 
-	    function MainController($scope, $firebaseAuth, $state) {
+	    function MainController($scope, $firebaseAuth, $state, NgMap) {
 	    	var vm = this;
 	    	var auth = $firebaseAuth();
 	    	var user = auth.$getAuth();
@@ -22,5 +22,22 @@
 			function routeTo(name) {
 				$state.go(name);
 			}
+
+			NgMap.getMap().then(function(map) {
+		    	var bangalore = { lat: 12.97, lng: 77.59 };
+
+		    	addMarker(bangalore, map)
+			});
+
+
+			function addMarker(location, map) {
+		        // Add the marker at the clicked location, and add the next-available label
+		        // from the array of alphabetical characters.
+		        var marker = new google.maps.Marker({
+		          position: location,
+		          label: 'test',
+		          map: map
+		        });
+		    }
 	    }
 })();
